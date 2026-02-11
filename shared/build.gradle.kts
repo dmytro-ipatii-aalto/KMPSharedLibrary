d9b206ec553e8b19a4b11a713074b8fdd0fb218d
Dmytro Ipatii <dmyto.ipatii@aalto.it>
2026-02-11 17:56:13 +0100

    Android package configuration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,14 +1,12 @@
-import com.android.tools.r8.internal.xc
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.android.lint)
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
-
-val libraryName = "KMPSharedLibrary"
-val libraryVersion = "1.0.0"
 
 kotlin {
 
@@ -16,9 +14,21 @@ kotlin {
     // which platforms this KMP module supports.
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
-        namespace = "com.example.shared"
+        namespace = "io.github.kmpsharedlibrary"
         compileSdk = 36
         minSdk = 24
+
+        
+        // Enables Java compilation support.
+        // This improves build times when Java compilation is not needed
+        withJava()
+
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+
+        }
+
 
         withHostTestBuilder {
         }
@@ -38,17 +48,20 @@ kotlin {
     // A step-by-step guide on how to include this library in an XCode
     // project can be found here:
     // https://developer.android.com/kotlin/multiplatform/migrate
+
+    val spmFrameworkName = "KMPSharedLibrary"
+
     val iOSTargets = listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     )
 
-    val xcFramework = XCFramework(xcFrameworkName = libraryName)
+    val xcFramework = XCFramework(xcFrameworkName = spmFrameworkName)
 
     iOSTargets.forEach { target ->
         target.binaries.framework {
-            baseName = libraryName
+            baseName = spmFrameworkName
             xcFramework.add(this)
             isStatic = true
         }
@@ -103,3 +116,65 @@ kotlin {
     }
 
 }
+
+group = "io.github"
+version = "1.0.0"
+
+publishing {
+    repositories {
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/dmytroipatii-hue/KMPSharedLibrary")
+            credentials(PasswordCredentials::class)
+
+//            credentials {
+//                username = providers.gradleProperty("githubPackagesUsername")
+//                    .orElse(providers.environmentVariable("USER_NAME"))
+//                    .get()
+//
+//                password = providers.gradleProperty("githubPackagesPassword")
+//                    .orElse(providers.environmentVariable("DEV_ACCESS_TOKEN"))
+//                    .get()
+//            }
+        }
+    }
+}
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        artifactId = "kmpsharedlibrary",
+    )
+
+
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("KMP Shared Library")
+        description.set("Sample Kotlin Multiplatform Library")
+        url.set("https://github.com/dmytroipatii-hue/KMPSharedLibrary")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("dmytroipatii-hue")
+                name.set("Dmytro Ipatii")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/dmytroipatii-hue/KMPSharedLibrary")
+            connection.set("scm:git:git://github.com/dmytroipatii-hue/KMPSharedLibrary.git")
+            developerConnection.set("scm:git:ssh://git@github.com/dmytroipatii-hue/KMPSharedLibrary.git")
+        }
+
+    }
+}
+
