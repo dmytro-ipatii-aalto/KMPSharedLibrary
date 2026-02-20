320d9d66d4dcddc58d571c57fed49af1d1c4a33f
Dmytro Ipatii <dmyto.ipatii@aalto.it>
2026-02-20 14:54:55 +0100

    Version 2.0.0

--- shared/src/commonMain/kotlin/com/example/shared/PlatformInfo.kt
+++ shared/src/commonMain/kotlin/com/example/shared/PlatformInfo.kt
@@ -12,7 +12,7 @@ public class PlatformInfo {
     }
 
     public fun getPlatformName(): String {
-        println("Version 1.0.0")
+        println("Version 2.0.0")
         println("Platform Name: ${platform.name}")
         return platform.name
     }
